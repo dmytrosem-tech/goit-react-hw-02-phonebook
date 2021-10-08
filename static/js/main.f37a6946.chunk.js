50b9c8be5b9d2fdf4074ff39a127f8116afbca9f
Dmytro Semeruk <deemoon83@gmail.com>
2021-10-08 09:24:21 +0300

    Updates

---
+++ static/js/main.f37a6946.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(9),n(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:"lol2"})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.f37a6946.chunk.js.map
